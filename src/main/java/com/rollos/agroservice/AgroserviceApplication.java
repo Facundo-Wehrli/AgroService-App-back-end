0ecfa6c58c489dd2d3d08fcf51042a84704e832e
Facundo <wehrlifacundo9@gmail.com>
2023-10-30 10:48:11 -0300

    initial commit

---
+++ src/main/java/com/rollos/agroservice/AgroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.rollos.agroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rollos/agroservice/AgroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rollos.agroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
